2b83ca7d583af6561e08b64c1442ec2b3a094985
Oliver Lyttleton <qtnzoly@addev.ucl.ac.uk>
2023-12-04 11:05:51 +0000

    tidy up api interactions

--- app/token/route.js
+++ app/token/route.js
@@ -5,11 +5,12 @@ export async function POST(request) {
 
   const requestData = await request.json()
 
-  const token = await getToken(requestData.username, requestData.password)
+  const token = await getToken(requestData.username, requestData.password, requestData.hostname)
 
   if (!!token) {
     cookies().set({name: 'token', value: token.access_token, secure:true, httpOnly: true})
     cookies().set({name: 'username', value: requestData.username, secure:true, httpOnly: true})
+    cookies().set({name: 'refererHost', value: requestData.hostname, secure:true, httpOnly: true})
   }
   return (!!token) ? new Response(JSON.stringify(token), {
     status: 200,
